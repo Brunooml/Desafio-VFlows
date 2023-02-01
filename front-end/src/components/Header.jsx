cdb96dddf8b3e2640863c65f9414d8146bb17e2b
Bruno <bml0401@gmail.com>
2023-02-01 14:03:19 -0300

    feat inicio da implementação da página de contratos e implementação do componente header

---
+++ front-end/src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React, { useContext } from 'react';
+import TestContext from '../context/TestContext';
+import contracts from '../data/contracts';
+
+function Header() {
+  const { cnpj } = useContext(TestContext);
+  return (
+    <div>
+      { contracts.filter((element) => element.header.cnpj == cnpj.cnpj).map((contrato, id) => {
+        return (
+          <div key={ id }>
+          <span>{ contrato.header.cnpj }</span>
+          <br />
+          <span>{ contrato.header.razao_social }</span>
+          <br />
+          <span>{ contrato.header.nome_fantasia }</span>
+          </div>
+        )
+      }) }
+    </div>
+  );
+}
+
+export default Header;
--- front-end/src/pages/Contratos.jsx
+++ front-end/src/pages/Contratos.jsx
@@ -1,5 +1,8 @@
 import React, { useContext } from 'react';
 import TestContext from '../context/TestContext';
+import Header from '../components/Header';
+import logo from '../data/logo.png'
+import contracts from '../data/contracts';
 
 
 function Contratos() {
@@ -7,8 +10,34 @@ function Contratos() {
 
   return (
     <div>
-      <h1>Contratos</h1>
-      <span> { cnpj.cnpj } </span>
+      <img src={ logo } width="100"/>
+      <h3>Pagamento de Fornecedor</h3>
+      <Header />
+      <h6>Contratos Vinculado</h6>
+      <table>
+      <thead>
+            <tr>
+                <th>Nome do Contrato</th>
+                <th>Código do Contrato</th>
+                <th>Retenção Técnica</th>
+                <th>Detalhes</th>
+            </tr>
+            </thead>
+            <tbody>
+              {
+                  contracts.filter((element) => element.header.cnpj == cnpj.cnpj).map((contrato, i) => (
+                    <tr key={ i }>
+                      <td>{contrato.lista_contratos.nome_contrato}</td>
+                      <td>{contrato.lista_contratos.codigo_contrato}</td>
+                      <td>{contrato.lista_contratos.percentual_retencao_tecnica}</td>
+                    </tr>
+                  ))
+              }
+            </tbody>
+      </table>
+
+      {console.log(contracts) }
+      { console.log(cnpj)}
     </div>
   )
 }
