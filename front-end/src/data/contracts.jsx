2c911106169f9d87b17ea4bce41aa9d71d553753
Bruno <bml0401@gmail.com>
2023-02-01 10:53:58 -0300

    feat: implementando a lista de contratos

---
+++ front-end/src/data/contracts.jsx
@@ -0,0 +1,164 @@
+const contracts = [
+  { header: {
+    cnpj: 12345678901234,
+    razao_social: "Razão Social do Fornecedor Logado",
+    nome_fantasia: "Nome Fantasia do Fornecedor Logado",
+  },
+  lista_contratos: [
+    { checkbox: "", nome_contrato: "Título do primeiro contrato de exemplo", codigo_contrato: "11002200-01", percentual_retencao_tecnica: "5%" },
+    { checkbox: "", nome_contrato: "Título do segundo contrato de exemplo", codigo_contrato: "11002200-02", percentual_retencao_tecnica: "10%" },
+    { checkbox: "", nome_contrato: "Título do terceiro contrato de exemplo", codigo_contrato: "11002200-03", percentual_retencao_tecnica: "5%" },
+    { checkbox: "", nome_contrato: "Título do quarto contrato de exemplo", codigo_contrato: "11002200-04", percentual_retencao_tecnica: "15%" },
+    { checkbox: "", nome_contrato: "Título do quinto contrato de exemplo", codigo_contrato: "11002200-05", percentual_retencao_tecnica: "5%" },
+    { checkbox: "", nome_contrato: "Título do sexto contrato de exemplo", codigo_contrato: "11002200-06", percentual_retencao_tecnica: "15%" },
+    { checkbox: "", nome_contrato: "Título do sétimo contrato de exemplo", codigo_contrato: "11002200-07", percentual_retencao_tecnica: "5%" },
+    { checkbox: "", nome_contrato: "Título do oitavo contrato de exemplo", codigo_contrato: "11002200-08", percentual_retencao_tecnica: "10%" },
+  ]
+  },
+  { header: {
+    cnpj: 12345678901235,
+    razao_social: "Razão Social do Fornecedor Logado",
+    nome_fantasia: "Nome Fantasia do Fornecedor Logado",
+  },
+  lista_contratos: [
+    { checkbox: "", nome_contrato: "Título do primeiro contrato de exemplo", codigo_contrato: "11002200-01", percentual_retencao_tecnica: "5%" },
+    { checkbox: "", nome_contrato: "Título do segundo contrato de exemplo", codigo_contrato: "11002200-02", percentual_retencao_tecnica: "10%" },
+    { checkbox: "", nome_contrato: "Título do terceiro contrato de exemplo", codigo_contrato: "11002200-03", percentual_retencao_tecnica: "5%" },
+    { checkbox: "", nome_contrato: "Título do quarto contrato de exemplo", codigo_contrato: "11002200-04", percentual_retencao_tecnica: "15%" },
+    { checkbox: "", nome_contrato: "Título do quinto contrato de exemplo", codigo_contrato: "11002200-05", percentual_retencao_tecnica: "5%" },
+    { checkbox: "", nome_contrato: "Título do sexto contrato de exemplo", codigo_contrato: "11002200-06", percentual_retencao_tecnica: "15%" },
+    { checkbox: "", nome_contrato: "Título do sétimo contrato de exemplo", codigo_contrato: "11002200-07", percentual_retencao_tecnica: "5%" },
+    { checkbox: "", nome_contrato: "Título do oitavo contrato de exemplo", codigo_contrato: "11002200-08", percentual_retencao_tecnica: "10%" },
+  ]
+  },
+  { header: {
+    cnpj: 12345678901236,
+    razao_social: "Razão Social do Fornecedor Logado",
+    nome_fantasia: "Nome Fantasia do Fornecedor Logado",
+  },
+  lista_contratos: [
+    { checkbox: "", nome_contrato: "Título do primeiro contrato de exemplo", codigo_contrato: "11002200-01", percentual_retencao_tecnica: "5%" },
+    { checkbox: "", nome_contrato: "Título do segundo contrato de exemplo", codigo_contrato: "11002200-02", percentual_retencao_tecnica: "10%" },
+    { checkbox: "", nome_contrato: "Título do terceiro contrato de exemplo", codigo_contrato: "11002200-03", percentual_retencao_tecnica: "5%" },
+    { checkbox: "", nome_contrato: "Título do quarto contrato de exemplo", codigo_contrato: "11002200-04", percentual_retencao_tecnica: "15%" },
+    { checkbox: "", nome_contrato: "Título do quinto contrato de exemplo", codigo_contrato: "11002200-05", percentual_retencao_tecnica: "5%" },
+    { checkbox: "", nome_contrato: "Título do sexto contrato de exemplo", codigo_contrato: "11002200-06", percentual_retencao_tecnica: "15%" },
+    { checkbox: "", nome_contrato: "Título do sétimo contrato de exemplo", codigo_contrato: "11002200-07", percentual_retencao_tecnica: "5%" },
+    { checkbox: "", nome_contrato: "Título do oitavo contrato de exemplo", codigo_contrato: "11002200-08", percentual_retencao_tecnica: "10%" },
+  ]
+  },
+  { header: {
+    cnpj: 12345678901237,
+    razao_social: "Razão Social do Fornecedor Logado",
+    nome_fantasia: "Nome Fantasia do Fornecedor Logado",
+  },
+  lista_contratos: [
+    { checkbox: "", nome_contrato: "Título do primeiro contrato de exemplo", codigo_contrato: "11002200-01", percentual_retencao_tecnica: "5%" },
+    { checkbox: "", nome_contrato: "Título do segundo contrato de exemplo", codigo_contrato: "11002200-02", percentual_retencao_tecnica: "10%" },
+    { checkbox: "", nome_contrato: "Título do terceiro contrato de exemplo", codigo_contrato: "11002200-03", percentual_retencao_tecnica: "5%" },
+    { checkbox: "", nome_contrato: "Título do quarto contrato de exemplo", codigo_contrato: "11002200-04", percentual_retencao_tecnica: "15%" },
+    { checkbox: "", nome_contrato: "Título do quinto contrato de exemplo", codigo_contrato: "11002200-05", percentual_retencao_tecnica: "5%" },
+    { checkbox: "", nome_contrato: "Título do sexto contrato de exemplo", codigo_contrato: "11002200-06", percentual_retencao_tecnica: "15%" },
+    { checkbox: "", nome_contrato: "Título do sétimo contrato de exemplo", codigo_contrato: "11002200-07", percentual_retencao_tecnica: "5%" },
+    { checkbox: "", nome_contrato: "Título do oitavo contrato de exemplo", codigo_contrato: "11002200-08", percentual_retencao_tecnica: "10%" },
+  ]
+  },
+  { header: {
+    cnpj: 12345678901238,
+    razao_social: "Razão Social do Fornecedor Logado",
+    nome_fantasia: "Nome Fantasia do Fornecedor Logado",
+  },
+  lista_contratos: [
+    { checkbox: "", nome_contrato: "Título do primeiro contrato de exemplo", codigo_contrato: "11002200-01", percentual_retencao_tecnica: "5%" },
+    { checkbox: "", nome_contrato: "Título do segundo contrato de exemplo", codigo_contrato: "11002200-02", percentual_retencao_tecnica: "10%" },
+    { checkbox: "", nome_contrato: "Título do terceiro contrato de exemplo", codigo_contrato: "11002200-03", percentual_retencao_tecnica: "5%" },
+    { checkbox: "", nome_contrato: "Título do quarto contrato de exemplo", codigo_contrato: "11002200-04", percentual_retencao_tecnica: "15%" },
+    { checkbox: "", nome_contrato: "Título do quinto contrato de exemplo", codigo_contrato: "11002200-05", percentual_retencao_tecnica: "5%" },
+    { checkbox: "", nome_contrato: "Título do sexto contrato de exemplo", codigo_contrato: "11002200-06", percentual_retencao_tecnica: "15%" },
+    { checkbox: "", nome_contrato: "Título do sétimo contrato de exemplo", codigo_contrato: "11002200-07", percentual_retencao_tecnica: "5%" },
+    { checkbox: "", nome_contrato: "Título do oitavo contrato de exemplo", codigo_contrato: "11002200-08", percentual_retencao_tecnica: "10%" },
+  ]
+  },
+  { header: {
+    cnpj: 12345678901239,
+    razao_social: "Razão Social do Fornecedor Logado",
+    nome_fantasia: "Nome Fantasia do Fornecedor Logado",
+  },
+  lista_contratos: [
+    { checkbox: "", nome_contrato: "Título do primeiro contrato de exemplo", codigo_contrato: "11002200-01", percentual_retencao_tecnica: "5%" },
+    { checkbox: "", nome_contrato: "Título do segundo contrato de exemplo", codigo_contrato: "11002200-02", percentual_retencao_tecnica: "10%" },
+    { checkbox: "", nome_contrato: "Título do terceiro contrato de exemplo", codigo_contrato: "11002200-03", percentual_retencao_tecnica: "5%" },
+    { checkbox: "", nome_contrato: "Título do quarto contrato de exemplo", codigo_contrato: "11002200-04", percentual_retencao_tecnica: "15%" },
+    { checkbox: "", nome_contrato: "Título do quinto contrato de exemplo", codigo_contrato: "11002200-05", percentual_retencao_tecnica: "5%" },
+    { checkbox: "", nome_contrato: "Título do sexto contrato de exemplo", codigo_contrato: "11002200-06", percentual_retencao_tecnica: "15%" },
+    { checkbox: "", nome_contrato: "Título do sétimo contrato de exemplo", codigo_contrato: "11002200-07", percentual_retencao_tecnica: "5%" },
+    { checkbox: "", nome_contrato: "Título do oitavo contrato de exemplo", codigo_contrato: "11002200-08", percentual_retencao_tecnica: "10%" },
+  ]
+  },
+  { header: {
+    cnpj: 12345678901230,
+    razao_social: "Razão Social do Fornecedor Logado",
+    nome_fantasia: "Nome Fantasia do Fornecedor Logado",
+  },
+  lista_contratos: [
+    { checkbox: "", nome_contrato: "Título do primeiro contrato de exemplo", codigo_contrato: "11002200-01", percentual_retencao_tecnica: "5%" },
+    { checkbox: "", nome_contrato: "Título do segundo contrato de exemplo", codigo_contrato: "11002200-02", percentual_retencao_tecnica: "10%" },
+    { checkbox: "", nome_contrato: "Título do terceiro contrato de exemplo", codigo_contrato: "11002200-03", percentual_retencao_tecnica: "5%" },
+    { checkbox: "", nome_contrato: "Título do quarto contrato de exemplo", codigo_contrato: "11002200-04", percentual_retencao_tecnica: "15%" },
+    { checkbox: "", nome_contrato: "Título do quinto contrato de exemplo", codigo_contrato: "11002200-05", percentual_retencao_tecnica: "5%" },
+    { checkbox: "", nome_contrato: "Título do sexto contrato de exemplo", codigo_contrato: "11002200-06", percentual_retencao_tecnica: "15%" },
+    { checkbox: "", nome_contrato: "Título do sétimo contrato de exemplo", codigo_contrato: "11002200-07", percentual_retencao_tecnica: "5%" },
+    { checkbox: "", nome_contrato: "Título do oitavo contrato de exemplo", codigo_contrato: "11002200-08", percentual_retencao_tecnica: "10%" },
+  ]
+  },
+  { header: {
+    cnpj: 12345678901231,
+    razao_social: "Razão Social do Fornecedor Logado",
+    nome_fantasia: "Nome Fantasia do Fornecedor Logado",
+  },
+  lista_contratos: [
+    { checkbox: "", nome_contrato: "Título do primeiro contrato de exemplo", codigo_contrato: "11002200-01", percentual_retencao_tecnica: "5%" },
+    { checkbox: "", nome_contrato: "Título do segundo contrato de exemplo", codigo_contrato: "11002200-02", percentual_retencao_tecnica: "10%" },
+    { checkbox: "", nome_contrato: "Título do terceiro contrato de exemplo", codigo_contrato: "11002200-03", percentual_retencao_tecnica: "5%" },
+    { checkbox: "", nome_contrato: "Título do quarto contrato de exemplo", codigo_contrato: "11002200-04", percentual_retencao_tecnica: "15%" },
+    { checkbox: "", nome_contrato: "Título do quinto contrato de exemplo", codigo_contrato: "11002200-05", percentual_retencao_tecnica: "5%" },
+    { checkbox: "", nome_contrato: "Título do sexto contrato de exemplo", codigo_contrato: "11002200-06", percentual_retencao_tecnica: "15%" },
+    { checkbox: "", nome_contrato: "Título do sétimo contrato de exemplo", codigo_contrato: "11002200-07", percentual_retencao_tecnica: "5%" },
+    { checkbox: "", nome_contrato: "Título do oitavo contrato de exemplo", codigo_contrato: "11002200-08", percentual_retencao_tecnica: "10%" },
+  ]
+  },
+  { header: {
+    cnpj: 12345678901232,
+    razao_social: "Razão Social do Fornecedor Logado",
+    nome_fantasia: "Nome Fantasia do Fornecedor Logado",
+  },
+  lista_contratos: [
+    { checkbox: "", nome_contrato: "Título do primeiro contrato de exemplo", codigo_contrato: "11002200-01", percentual_retencao_tecnica: "5%" },
+    { checkbox: "", nome_contrato: "Título do segundo contrato de exemplo", codigo_contrato: "11002200-02", percentual_retencao_tecnica: "10%" },
+    { checkbox: "", nome_contrato: "Título do terceiro contrato de exemplo", codigo_contrato: "11002200-03", percentual_retencao_tecnica: "5%" },
+    { checkbox: "", nome_contrato: "Título do quarto contrato de exemplo", codigo_contrato: "11002200-04", percentual_retencao_tecnica: "15%" },
+    { checkbox: "", nome_contrato: "Título do quinto contrato de exemplo", codigo_contrato: "11002200-05", percentual_retencao_tecnica: "5%" },
+    { checkbox: "", nome_contrato: "Título do sexto contrato de exemplo", codigo_contrato: "11002200-06", percentual_retencao_tecnica: "15%" },
+    { checkbox: "", nome_contrato: "Título do sétimo contrato de exemplo", codigo_contrato: "11002200-07", percentual_retencao_tecnica: "5%" },
+    { checkbox: "", nome_contrato: "Título do oitavo contrato de exemplo", codigo_contrato: "11002200-08", percentual_retencao_tecnica: "10%" },
+  ]
+  },
+  { header: {
+    cnpj: 12345678901233,
+    razao_social: "Razão Social do Fornecedor Logado",
+    nome_fantasia: "Nome Fantasia do Fornecedor Logado",
+  },
+  lista_contratos: [
+    { checkbox: "", nome_contrato: "Título do primeiro contrato de exemplo", codigo_contrato: "11002200-01", percentual_retencao_tecnica: "5%" },
+    { checkbox: "", nome_contrato: "Título do segundo contrato de exemplo", codigo_contrato: "11002200-02", percentual_retencao_tecnica: "10%" },
+    { checkbox: "", nome_contrato: "Título do terceiro contrato de exemplo", codigo_contrato: "11002200-03", percentual_retencao_tecnica: "5%" },
+    { checkbox: "", nome_contrato: "Título do quarto contrato de exemplo", codigo_contrato: "11002200-04", percentual_retencao_tecnica: "15%" },
+    { checkbox: "", nome_contrato: "Título do quinto contrato de exemplo", codigo_contrato: "11002200-05", percentual_retencao_tecnica: "5%" },
+    { checkbox: "", nome_contrato: "Título do sexto contrato de exemplo", codigo_contrato: "11002200-06", percentual_retencao_tecnica: "15%" },
+    { checkbox: "", nome_contrato: "Título do sétimo contrato de exemplo", codigo_contrato: "11002200-07", percentual_retencao_tecnica: "5%" },
+    { checkbox: "", nome_contrato: "Título do oitavo contrato de exemplo", codigo_contrato: "11002200-08", percentual_retencao_tecnica: "10%" },
+  ]
+  },
+]
+
+export default contracts
